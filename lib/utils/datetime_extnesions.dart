9aef64dec57ab8ad6c72884dcd2d681aa86c2312
Francesco De Salvo <fra.desalvo.02@gmail.com>
2023-01-08 17:58:18 +0100

    datetime compare extension

---
+++ lib/utils/datetime_extnesions.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+extension DateComparison on DateTime {
+  bool operator >(other) => compareTo(other) > 0;
+  bool operator <(other) => compareTo(other) < 0;
+  bool operator >=(other) => compareTo(other) >= 0;
+  bool operator <=(other) => compareTo(other) <= 0;
+}
+
+extension TimeComparison on TimeOfDay {
+  int compareTo(TimeOfDay other) {
+    if (hour < other.hour) return -1;
+    if (hour > other.hour) return 1;
+    if (minute < other.minute) return -1;
+    if (minute > other.minute) return 1;
+    return 0;
+  }
+
+  bool operator >(other) => compareTo(other) > 0;
+  bool operator <(other) => compareTo(other) < 0;
+  bool operator >=(other) => compareTo(other) >= 0;
+  bool operator <=(other) => compareTo(other) <= 0;
+}
