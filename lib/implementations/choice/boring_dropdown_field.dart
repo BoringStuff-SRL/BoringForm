83cc748aa36253878918083afe3d22184f1d40c2
Leo <agama1257@gmail.com>
2023-12-20 17:30:57 +0100

    implementing dropdown

--- lib/implementations/choice/boring_dropdown_field.dart
+++ lib/implementations/choice/boring_dropdown_field.dart
@@ -1,113 +1,51 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
-import 'package:boring_form/field/boring_field.dart';
-import 'package:boring_form/field/boring_field_controller.dart';
-import 'package:boring_form/theme/boring_field_decoration.dart';
+import 'package:boring_dropdown/boring_dropdown.dart';
+import 'package:boring_form/field/boring_form_field_with_async_calculations.dart';
+import 'package:boring_form/form/boring_form_controller.dart';
+import 'package:boring_form/implementations/choice/boring_radiogroup_field.dart';
 import 'package:boring_form/theme/boring_form_theme.dart';
-import 'package:boring_form/theme/boring_responsive_size.dart';
-import 'package:dropdown_button2/dropdown_button2.dart';
 import 'package:flutter/material.dart';
 
-class BoringDropDownField<T> extends BoringField<T> {
-  BoringDropDownField(
-      {super.key,
-      required super.jsonKey,
-      required this.items,
-      this.radius = 0,
-      super.fieldController,
-      super.decoration,
-      super.displayCondition,
-      super.boringResponsiveSize,
-      bool? readOnly,
-      super.onChanged,
-      this.showEraseValueButton = true})
-      : super(readOnly: readOnly);
+class BoringDropdownField<T>
+    extends BoringFormFieldWithAsyncCalculations<T, List<BoringChoiceItem<T>>> {
+  const BoringDropdownField({
+    super.key,
+    required super.fieldPath,
+    required this.getItems,
+    super.decoration,
+    super.observedFields,
+    super.readOnly,
+    super.validationFunction,
+  });
 
-  final List<DropdownMenuItem<T?>> items;
-  final double radius;
-  final bool showEraseValueButton;
+  final Future<List<BoringChoiceItem<T>>> getItems;
 
   @override
-  Widget builder(context, controller, child) {
-    final style = BoringFormTheme.of(context).style;
-
-    return BoringField.boringFieldBuilder(
-      style,
-      decoration?.label,
-      child: ValueListenableBuilder(
-          valueListenable: controller.autoValidate
-              ? ValueNotifier(false)
-              : controller.hideError,
-          builder: (BuildContext context, bool value, Widget? child) {
-            final InputDecoration newStyle =
-                getDecoration(context, haveError: value)
-                    .copyWith(contentPadding: const EdgeInsets.all(0));
-
-            return Row(
-              children: [
-                Expanded(
-                  child: DropdownButtonFormField2<T?>(
-                    dropdownOverButton: false,
-                    isExpanded: true,
-                    dropdownElevation: 0,
-                    decoration: newStyle,
-                    buttonHeight: 51,
-                    itemHeight: 50,
-                    dropdownMaxHeight: 250,
-                    items: items,
-                    value: controller.value,
-                    hint: Text(
-                      decoration?.hintText ?? '',
-                      style: style.inputDecoration.hintStyle,
-                    ),
-                    dropdownDecoration: _boxDecoration(newStyle),
-                    onChanged: isReadOnly(context)
-                        ? null
-                        : ((value) {
-                            controller.value = value;
-                          }),
-                  ),
-                ),
-                if (showEraseValueButton && controller.value != null) ...[
-                  const SizedBox(width: 5),
-                  eraseButtonWidget(context, style.eraseValueWidget),
-                ],
-              ],
-            );
-          }),
+  Widget builder(
+      BuildContext context,
+      BoringFormTheme formTheme,
+      BoringFormController formController,
+      T? fieldValue,
+      String? errror,
+      AsyncSnapshot<List<BoringChoiceItem<T>>> calculations) {
+    return BoringDropdown<T>(
+      items: calculations.data!
+          .map((e) => DropdownMenuItem(value: e.value, child: Text(e.display)))
+          .toList(),
+      multichoice: false,
+      convertItemToString: (element) =>
+          calculations.data!.firstWhere((el) => element == el).display,
+      onChanged: (val) {
+        setChangedValue(formController, val);
+      },
+      value: fieldValue,
     );
   }
 
-  _boxDecoration(InputDecoration newStyle) => BoxDecoration(
-      borderRadius: BorderRadius.circular(radius),
-      color: newStyle.fillColor ?? Colors.white,
-      border: Border.all(
-          color: newStyle.border?.borderSide.color ?? Colors.grey,
-          width: newStyle.border?.borderSide.width ?? 1));
+  @override
+  Future<List<BoringChoiceItem<T>>> onObservedFieldsChange(
+          BoringFormController formController) =>
+      getItems;
 
-  // @override
-  // void onValueChanged(T? newValue) {}
-
-  // @override
-  // BoringDropDownField copyWith({
-  //   BoringFieldController<T>? fieldController,
-  //   void Function(T? p1)? onChanged,
-  //   BoringFieldDecoration? decoration,
-  //   BoringResponsiveSize? boringResponsiveSize,
-  //   String? jsonKey,
-  //   bool Function(Map<String, dynamic> p1)? displayCondition,
-  //   List<DropdownMenuItem<dynamic>>? items,
-  //   InputDecoration? searchInputDecoration,
-  //   double? radius,
-  // }) {
-  //   return BoringDropDownField(
-  //     boringResponsiveSize: boringResponsiveSize ?? this.boringResponsiveSize,
-  //     jsonKey: jsonKey ?? this.jsonKey,
-  //     decoration: decoration ?? this.decoration,
-  //     onChanged: (onChanged as void Function(dynamic)?) ??
-  //         (this.onChanged as void Function(dynamic)?),
-  //     displayCondition: displayCondition ?? this.displayCondition,
-  //     fieldController: fieldController ?? this.fieldController,
-  //     items: items ?? this.items,
-  //   );
-  // }
+  @override
+  void onSelfChange(BoringFormController formController, T? fieldValue) {}
 }
