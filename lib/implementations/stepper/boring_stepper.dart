8b27c75f9206dcf6fabd2b9fec2ec3881f25a7ca
Leonardo Valente <agama1257@gmail.com>
2024-10-07 08:44:55 +0200

    fix stepper style

--- lib/implementations/stepper/boring_stepper.dart
+++ lib/implementations/stepper/boring_stepper.dart
@@ -30,79 +30,84 @@ class BoringStepper extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListenableBuilder(
-      listenable: stepperController,
-      builder: (context, child) => Stepper(
-        type: stepperStyle.stepperType,
-        currentStep: stepperController.activeStepIndex,
-        controlsBuilder: (context, details) {
-          return Row(
-            children: [
-              if (canGoBack)
-                FilledButton.tonal(
-                  onPressed: () {
-                    details.onStepCancel!.call();
-                  },
-                  style: stepperStyle.onCancelButtonStyle,
-                  child: Text(stepperStyle.onCancelText),
-                ),
-              const SizedBox(width: 5),
-              if (canContinue)
-                FilledButton(
-                  onPressed: () {
-                    details.onStepContinue!.call();
-                  },
-                  style: stepperStyle.onContinueButtonStyle,
-                  child: Text(stepperStyle.onContinueText),
-                ),
-            ],
-          );
-        },
-        onStepContinue: () {
-          if (canContinue) {
-            if (onStepContinue != null) {
-              onStepContinue?.call(stepperController.activeStepIndex + 1);
-            } else {
-              stepperController.nextStep();
-            }
-
-            // questa cosa mi esegue entrambe le chiamate
-            // onStepContinue?.call(stepperController.activeStepIndex + 1) ??
-            //     stepperController.nextStep();
-          }
-        },
-        onStepCancel: () {
-          if (onStepCancel != null) {
-            onStepCancel?.call(max(0, stepperController.activeStepIndex - 1));
-          } else {
-            stepperController.previousStep();
-          }
-        },
-        onStepTapped: (value) {
-          if (onStepTapped != null) {
-            onStepTapped?.call(value);
-          } else {
-            stepperController.changeStepIndex(value);
-          }
-        },
-        steps: steps.map(
-          (e) {
-            final isActive = stepperController.isStepActive(steps.indexOf(e));
-
-            return Step(
-              title: Text(e.title, style: stepperStyle.titleTextStyle),
-              content: e.child,
-              isActive: isActive,
-              state: isActive ? StepState.editing : StepState.indexed,
-              subtitle: e.subTitle == null
-                  ? null
-                  : Text(
-                      e.subTitle!,
-                      style: stepperStyle.subTitleTextStyle,
-                    ),
+    final theme = Theme.of(context);
+    return Theme(
+      data: theme.copyWith(canvasColor: Colors.transparent),
+      child: ListenableBuilder(
+        listenable: stepperController,
+        builder: (context, child) => Stepper(
+          type: stepperStyle.stepperType,
+          currentStep: stepperController.activeStepIndex,
+          elevation: 0.0,
+          controlsBuilder: (context, details) {
+            return Row(
+              children: [
+                if (canGoBack)
+                  FilledButton.tonal(
+                    onPressed: () {
+                      details.onStepCancel!.call();
+                    },
+                    style: stepperStyle.onCancelButtonStyle,
+                    child: Text(stepperStyle.onCancelText),
+                  ),
+                const SizedBox(width: 5),
+                if (canContinue)
+                  FilledButton(
+                    onPressed: () {
+                      details.onStepContinue!.call();
+                    },
+                    style: stepperStyle.onContinueButtonStyle,
+                    child: Text(stepperStyle.onContinueText),
+                  ),
+              ],
             );
           },
-        ).toList(),
+          onStepContinue: () {
+            if (canContinue) {
+              if (onStepContinue != null) {
+                onStepContinue?.call(stepperController.activeStepIndex + 1);
+              } else {
+                stepperController.nextStep();
+              }
+
+              // questa cosa mi esegue entrambe le chiamate
+              // onStepContinue?.call(stepperController.activeStepIndex + 1) ??
+              //     stepperController.nextStep();
+            }
+          },
+          onStepCancel: () {
+            if (onStepCancel != null) {
+              onStepCancel?.call(max(0, stepperController.activeStepIndex - 1));
+            } else {
+              stepperController.previousStep();
+            }
+          },
+          onStepTapped: (value) {
+            if (onStepTapped != null) {
+              onStepTapped?.call(value);
+            } else {
+              stepperController.changeStepIndex(value);
+            }
+          },
+          steps: steps.map(
+            (e) {
+              final isActive = stepperController.isStepActive(steps.indexOf(e));
+
+              return Step(
+                title: Text(e.title, style: stepperStyle.titleTextStyle),
+                content: e.child,
+                isActive: isActive,
+                state: isActive ? StepState.editing : StepState.indexed,
+                subtitle: e.subTitle == null
+                    ? null
+                    : Text(
+                        e.subTitle!,
+                        style: stepperStyle.subTitleTextStyle,
+                      ),
+              );
+            },
+          ).toList(),
+        ),
       ),
     );
   }
