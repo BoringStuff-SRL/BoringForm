e7bddd6c2ee5955a44d7ee74eddeb5fee0d80fd5
fradesa <fra.desalvo.02@gmail.com>
2025-03-20 17:45:09 +0100

    Added required param to CheckBox

--- lib/implementations/choice/boring_checkbox_field.dart
+++ lib/implementations/choice/boring_checkbox_field.dart
@@ -14,6 +14,7 @@ class BoringCheckBoxField extends BFormField<bool> {
     this.mainAxisAlignment,
     this.unCheckColor,
     super.onChanged,
+    super.required,
   });
 
   final Color? unCheckColor;
